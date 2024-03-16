ca670ed908eb0da75360ad36af2f505da6abb0c8
Nitin Kanish <nitinkanish@gmail.com>
2024-03-16 13:06:58 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,17 +8,17 @@ import AppStore from "../../public/img/images/join/appStore.png";
 const Footer = () => {
   return (
     <>
-      <footer class="footer">
-        <div class="container footer__container">
-          <div class="footer__inner">
+      <footer className="footer">
+        <div className="container footer__container">
+          <div className="footer__inner">
             <div>
-              <a href="./" class="footer-logo" title="litit logo">
+              <a href="./" className="footer-logo" title="litit logo">
                 <picture>
                   {/* <img src="assets/img/icons/logo.svg" width="109" height="44" alt="" /> */}
                   <Image src={FooterLogo} alt="" height={44} width={109} />
                 </picture>
               </a>
-              <ul class="footer__socials">
+              <ul className="footer__socials">
                 <li>
                   <a
                     href="https://www.linkedin.com/company/lititapp"
@@ -26,7 +26,6 @@ const Footer = () => {
                     title="litit linkedin"
                   >
                     <picture>
-                      {/* <img src="assets/img/icons/footer/linkedin.svg" width="20" height="20" alt="" /> */}
                       <Image src={Linkedin} alt="" height={20} width={20} />
                     </picture>
                   </a>
@@ -34,9 +33,9 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div class="footer-links">
-              <ul class="footer-links__list">
-                <li class="footer-links__item">
+            <div className="footer-links">
+              <ul className="footer-links__list">
+                <li className="footer-links__item">
                   <a
                     href="./pages/privacy-notice"
                     target="_blank"
@@ -47,14 +46,14 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div class="footer__store-btn">
+            <div className="footer__store-btn">
               <a
                 href="https://apps.apple.com/us/app/lit-it-short-video-app/id1610743469"
                 data-download-link="true"
                 target="_blank"
               >
                 {/* <img src="assets/img/images/join/appStore.png" loading="lazy" alt="" class="store-img"> */}
-                <Image src={AppStore} alt="" class="store-img" />
+                <Image src={AppStore} alt="" className="store-img" />
               </a>
               <a
                 href="https://play.google.com/store/apps/details?id=com.litit.app"
@@ -62,7 +61,7 @@ const Footer = () => {
                 target="_blank"
               >
                 {/* <img src="assets/img/images/join/playMarket.png" loading="lazy" alt="" class="store-img"> */}
-                <Image src={PlayMarket} alt="" class="store-img" />
+                <Image src={PlayMarket} alt="" className="store-img" />
               </a>
             </div>
           </div>
