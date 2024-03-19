31fec1c2c0a66a3d616ce5549dc396b9f27ba152
Nitin Kanish <nitinkanish@gmail.com>
2024-03-20 02:18:01 +0530

    changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,19 @@
+"use client" 
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 import ProfileIndex from '@/components/profile';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 
 const Profile = () => {
+    const router = useRouter();
+    useEffect(() => {
+        const token = localStorage.getItem('jwt');
+        if (!token) {
+            router.push('/signin'); // Redirect to signin page if token not found
+        }
+    }, [])
     return(
         <>
         <Header />
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
 import SignInCom from '@/components/signincom';
+import { useRouter } from 'next/navigation';
 
 export default function Signin () {
-
+    
+    
+        const router = useRouter();
+        useEffect(() => {
+            const token = localStorage.getItem('jwt');
+            if (token) {
+                router.push('/profile'); // Redirect to signin page if token not found
+            }
+        }, [])
     return (
         <>
           
